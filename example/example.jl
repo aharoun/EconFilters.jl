b37c8a3bf98e0b5cfe080c6e87a6a389670bd026
Harun Alp <harun.alp@gmail.com>
2019-01-08 17:21:21 -0500

    hp filter with extended series

--- example/example.jl
+++ example/example.jl
@@ -18,5 +18,20 @@ cOpt,tOpt = hpfilter(y,λOpt)
 # bkfilter(data,pL,pU,L) where pL and pU are lower and upper periods for bandpass filter, L is the order of the symmetric moving average
 cBK =  bkfilter(y,6,32,12)
 
-plot(data[:date],[c cOpt cBK],label=["Hp1600","Hp$λOpt","BK"],legend=:bottomright,xtickfont = font(5))
+
+# To reduce end-of-sample bias of hp filter: Estimate an arima model, extend y series with forecasted values.
+# For this, use SSM package (unregistered).
+# First add the package
+# ```add https://github.com/aharoun/SSM.jl```
+#
+using SSM
+model = arima(2,1,2)
+modelEst, estParams , _ = estimate(model, y);
+
+fH = 10  # forecast horizon (period)
+yF, _ = forecast(modelEst, y, fH)
+
+cExt, tExt = hpfilter([y;yF],1600)
+
+plot(data[:date],[c cExt[1:end-fH] cOpt cBK],label=["Hp1600","Hp1600 Extended","Hp$λOpt","BK"],legend=:bottomright,xtickfont = font(5))
 
